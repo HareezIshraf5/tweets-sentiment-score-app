8e15cbf08da67dee0a380c52cc89fe62afb68e45
fahmi fakhir <61687142+fahmifakhir@users.noreply.github.com>
2023-01-30 01:42:44 +0800

    Update my_textfield.dart

--- lib/components/my_textfield.dart
+++ lib/components/my_textfield.dart
@@ -15,21 +15,22 @@ class MyTextField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      padding: const EdgeInsets.symmetric(horizontal: 60.0),
       child: TextField(
         controller: controller,
         obscureText: obscureText,
         decoration: InputDecoration(
+            border:OutlineInputBorder(borderRadius: BorderRadius.circular(30)),
             enabledBorder: const OutlineInputBorder(
               borderSide: BorderSide(color: Colors.white),
             ),
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.grey.shade400),
+            focusedBorder: const OutlineInputBorder(      
+              borderSide: BorderSide(color: Colors.white),
             ),
-            fillColor: Colors.grey.shade200,
+            fillColor: Colors.white,
             filled: true,
             hintText: hintText,
-            hintStyle: TextStyle(color: Colors.grey[500])),
+            hintStyle: TextStyle(color: Colors.grey[400])),
       ),
     );
   }
