320eba529055b81733be62b36a318245d63bcbdb
aminnurrasyid <57680454+aminnurrasyid@users.noreply.github.com>
2023-02-12 17:53:14 +0800

    unusable api comment

--- lib/api.dart
+++ lib/api.dart
@@ -1,5 +1,9 @@
 import 'package:http/http.dart' as http;
 
+// this API call towards our hosted API currently will not return anything since it depends on Twitter API, which is currently changed to a paid plan.
+// further evaluation process can be made by checking our hosted API github repo
+// find it here : https://github.com/aminnurrasyid/tweet-sentimentscore-api
+
 Future Getdata(url) async {
   http.Response Response = await http.get(url);
   return Response.body;
