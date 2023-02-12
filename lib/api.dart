6c5badd04057dd0bf7d5aa51347a5fbfcd29ba45
aminnurrasyid <57680454+aminnurrasyid@users.noreply.github.com>
2023-02-12 18:16:51 +0800

    unusable api comment

--- lib/api.dart
+++ lib/api.dart
@@ -1,6 +1,8 @@
 import 'package:http/http.dart' as http;
 
-// this API call towards our hosted API currently will not return anything since it depends on Twitter API, which is currently changed to a paid plan.
+// in case this api does not work anymore this mean that;
+// this API call towards our hosted API currently will not return anything since it depends on Twitter API, which is currently changed to a paid plan. 
+// Twitter claimed that it will be changed on (13 February 2023)
 // further evaluation process can be made by checking our hosted API github repo
 // find it here : https://github.com/aminnurrasyid/tweet-sentimentscore-api
 
