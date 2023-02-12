6e44a124b705ba6db862acc421a818270355edc9
aminnurrasyid <57680454+aminnurrasyid@users.noreply.github.com>
2023-02-12 18:18:28 +0800

    Update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -66,7 +66,9 @@ class _HomePageState extends State<HomePage> {
                   ),
                   TextField(
                     onChanged: (value) {
+                      // if this API does not work this mean that :-
                       // this API call towards our hosted API currently will not return anything since it depends on Twitter API, which is currently changed to a paid plan.
+                      // Twitter claim that it will be changed on 13 February 2023
                       // the call function can be referred to api.dart
                       // further evaluation process can be made by checking our hosted API github repo
                       // find it here : https://github.com/aminnurrasyid/tweet-sentimentscore-api
